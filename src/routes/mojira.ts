d366f15c1f5f247863ce1f8fe4d172b3a4454c89
Dianliang233 <dianliang233@gmail.com>
2024-05-26 21:27:56 +0800

    Use one request each for Java and Bedrock

--- src/routes/mojira.ts
+++ src/routes/mojira.ts
@@ -1,6 +1,5 @@
 import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
-import { JSONValue } from 'hono/utils/types'
 import { z } from 'zod'
 
 const app = new Hono()
@@ -13,37 +12,53 @@ const querySchema = z.object({
 app.get('/', zValidator('query', querySchema), async (c) => {
   const { search, version } = c.req.valid('query')
 
-  const jql = `
-    project in (MC, MCPE)
+  const jqls = [
+    `
+    project = MC
     AND "Confirmation Status" != Unconfirmed
     AND (
       resolution IS EMPTY
       OR (
         resolution = Fixed
-        AND (
-          affectedVersion = "${version}"
-          OR fixVersion = earliestUnreleasedVersion()
-        )
+        AND affectedVersion = "${version}"
       )
     )
     AND (summary ~ "${search}")
     ORDER BY
       resolution ASC,
-      "Mojang Priority" ASC
-  `
-    .replace(/\n/g, ' ')
-    .replace(/\s+/g, ' ')
+      "Mojang Priority" ASC,
+      votes DESC,
+      key ASC
+    `,
+    `
+    project = MCPE
+    AND "Confirmation Status" != Unconfirmed
+    AND resolution IS EMPTY
+    AND (summary ~ "${search}")
+    ORDER BY
+      resolution ASC,
+      votes DESC,
+      key ASC
+    `,
+  ].map((s) => s.trim().replace(/\n/g, ' ').replace(/\s+/g, ' '))
 
-  const req = await fetch(
-    `https://bugs.mojang.com/rest/api/2/search?maxResults=20&fields=summary&jql=${encodeURIComponent(jql)}`,
-    {
-      headers: {
-        'User-Agent': 'MCWJigsawProxy (+https://github.com/mc-wiki/jigsaw)',
-      },
-    },
+  const req = await Promise.all(
+    jqls.map((jql) =>
+      fetch(
+        `https://bugs.mojang.com/rest/api/2/search?maxResults=10&fields=summary&jql=${encodeURIComponent(jql)}`,
+        {
+          headers: {
+            'User-Agent': 'MCWJigsawProxy (+https://github.com/mc-wiki/jigsaw)',
+          },
+        },
+      ),
+    ),
   )
 
-  return c.json((await req.json()) as JSONValue)
+  return c.json({
+    java: await req[0].json(),
+    bedrock: await req[1].json(),
+  })
 })
 
 export default app
