1400003911fa4bd8d99005a6fea483d623b458e4
Dianliang233 <dianliang233@gmail.com>
2025-02-19 18:59:40 +0800

    Adapt to new Mojira API

--- src/routes/mojira.ts
+++ src/routes/mojira.ts
@@ -10,20 +10,27 @@ app.use(async (c, next) => {
 })
 
 const querySchema = z.object({
+  project: z.string().min(1),
   jql: z.string().min(1),
 })
 
 app.get('/', zValidator('query', querySchema), async (c) => {
-  const { jql } = c.req.valid('query')
+  const { project, jql } = c.req.valid('query')
 
-  const req = await fetch(
-    `https://bugs.mojang.com/rest/api/2/search?maxResults=10&fields=summary,resolution&jql=${encodeURIComponent(jql)}`,
-    {
-      headers: {
-        'User-Agent': 'minecraft.wiki API Proxy (dianliang233 at gmail.com)',
-      },
+  const req = await fetch('https://bugs.mojang.com/api/jql-search-post', {
+    method: 'POST',
+    headers: {
+      'User-Agent': 'minecraft.wiki API Proxy (dianliang233 at gmail.com)',
+      'Content-Type': 'application/json',
     },
-  )
+    body: JSON.stringify({
+      advanced: true,
+      search: jql,
+      project: project,
+      startAt: 0,
+      maxResults: 10,
+    }),
+  })
 
   return c.json((await req.json()) as object)
 })
